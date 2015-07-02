22164d404ca45679a814e715abe7daf23581973e
rufer7 <marc.rufer@d-fens.net>
2015-07-02 13:00:52 +0200

    Initial commit

---
+++ biz.dfch.CS.JustMock.Examples.Tests/StaticMockingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+
+namespace biz.dfch.CS.JustMock.Examples.Tests
+{
+    [TestClass]
+    public class StaticMockingTests
+    {
+        [TestMethod]
+        public void MockedConsoleWriteLineGettingCalledWithSpecificArgument()
+        {
+            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
+            var called = false;
+            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
+            Console.WriteLine("Test");
+            Assert.IsTrue(called);
+        }
+
+        [TestMethod]
+        public void MockedConsoleWriteLineNotGettingCalledWithSpecificArgument()
+        {
+            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
+            var called = false;
+            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
+            Console.WriteLine("Something");
+            Assert.IsFalse(called);
+        }
+    }
+}
