6e8a00c6f1eaa7e1f9785f7c82714de4f6f05a54
rufer7 <marc.rufer@d-fens.net>
2015-07-02 15:50:05 +0200

    rearranged tests

--- biz.dfch.CS.JustMock.Examples.Tests/BasicStaticMockingTests.cs
+++ biz.dfch.CS.JustMock.Examples.Tests/BasicStaticMockingTests.cs
@@ -20,15 +20,8 @@ using Telerik.JustMock;
 namespace biz.dfch.CS.JustMock.Examples.Tests
 {
     [TestClass]
-    public class StaticMockingTests
+    public class BasicStaticMockingTests
     {
-        private Boolean called;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            called = false;
-        }
 
         /**
          * When mocking DateTimeOffset it's important to create the mock with the 
@@ -44,34 +37,6 @@ namespace biz.dfch.CS.JustMock.Examples.Tests
             Mock.Arrange(() => DateTimeOffset.UtcNow).Returns(new DateTimeOffset(2015, 1, 1, 3, 10, 7, new TimeSpan()));
 
             Assert.AreEqual(new DateTimeOffset(2015, 1, 1, 3, 10, 7, new TimeSpan()), DateTimeOffset.UtcNow);
-        }
-
-
-        /**
-         * If passing a specific parameter like "Test" in mock arrangment the mocked method will only be called
-         * if the parameter in the call (Act) matches the parameter specified in arrangement.
-         **/
-
-        [TestMethod]
-        public void MockedConsoleWriteLineGettingCalledWithSpecificArgument()
-        {
-            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
-            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
-            
-            Console.WriteLine("Test");
-            
-            Assert.IsTrue(called);
-        }
-        
-        [TestMethod]
-        public void MockedConsoleWriteLineNotGettingCalledWithSpecificArgument()
-        {
-            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
-            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
-
-            Console.WriteLine("Something");
-
-            Assert.IsFalse(called);
         }
     }
 }
---
+++ biz.dfch.CS.JustMock.Examples.Tests/MustBeCalledWithMatchingTest.cs
@@ -0,0 +1,60 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/**
+ * Copyright 2015 Marc Rufer, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+﻿using System;
+using Telerik.JustMock;
+
+namespace biz.dfch.CS.JustMock.Examples.Tests
+{
+    [TestClass]
+    public class MustBeCalledWithMatchingTest
+    {
+        private Boolean _called;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _called = false;
+        }
+
+         /**
+         * If passing a specific parameter like "Test" in mock arrangment the mocked method will only be called
+         * if the parameter in the call (Act) matches the parameter specified in arrangement.
+         **/
+
+        [TestMethod]
+        public void MockedConsoleWriteLineGettingCalledWithSpecificArgument()
+        {
+            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
+            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { _called = true; }).MustBeCalled();
+
+            Console.WriteLine("Test");
+
+            Assert.IsTrue(_called);
+        }
+
+        [TestMethod]
+        public void MockedConsoleWriteLineNotGettingCalledWithSpecificArgument()
+        {
+            Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
+            Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { _called = true; }).MustBeCalled();
+
+            Console.WriteLine("Something");
+
+            Assert.IsFalse(_called);
+        }
+    }
+}
