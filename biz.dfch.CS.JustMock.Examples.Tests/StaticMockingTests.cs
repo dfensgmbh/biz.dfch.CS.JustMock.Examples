304017aa57d5af8b0eba43a11a8bb3341bbb9c2d
rufer7 <marc.rufer@d-fens.net>
2015-07-02 14:33:11 +0200

    test for DateTimeOffset added

--- biz.dfch.CS.JustMock.Examples.Tests/StaticMockingTests.cs
+++ biz.dfch.CS.JustMock.Examples.Tests/StaticMockingTests.cs
@@ -7,13 +7,44 @@ namespace biz.dfch.CS.JustMock.Examples.Tests
     [TestClass]
     public class StaticMockingTests
     {
+        private Boolean called;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            called = false;
+        }
+
+        /**
+         * When mocking DateTimeOffset it's important to create the mock with the 
+         * Behavior.CallOriginal behaviour, if DateTimeOffset is used in arrangement.
+         * 
+         * Otherwise a default value will be returned because by default the Behaviour is set to RecursiveLooser as described here:
+         * http://www.telerik.com/help/justmock/basic-usage-mock-behaviors.html
+         **/
+        [TestMethod]
+        public void MockedDateTimeOffsetReturnsExpectedDateTimeOffset()
+        {
+            Mock.SetupStatic(typeof(DateTimeOffset), Behavior.CallOriginal, StaticConstructor.Mocked);
+            Mock.Arrange(() => DateTimeOffset.UtcNow).Returns(new DateTimeOffset(2015, 1, 1, 3, 10, 7, new TimeSpan()));
+
+            Assert.AreEqual(new DateTimeOffset(2015, 1, 1, 3, 10, 7, new TimeSpan()), DateTimeOffset.UtcNow);
+        }
+
+
+        /**
+         * If passing a specific parameter like "Test" in mock arrangment the mocked method will only be called
+         * if the parameter in the call (Act) matches the parameter specified in arrangement.
+         **/
+
         [TestMethod]
         public void MockedConsoleWriteLineGettingCalledWithSpecificArgument()
         {
             Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
-            var called = false;
             Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
+            
             Console.WriteLine("Test");
+            
             Assert.IsTrue(called);
         }
 
@@ -21,9 +52,10 @@ namespace biz.dfch.CS.JustMock.Examples.Tests
         public void MockedConsoleWriteLineNotGettingCalledWithSpecificArgument()
         {
             Mock.SetupStatic(typeof(Console), StaticConstructor.Mocked);
-            var called = false;
             Mock.Arrange(() => Console.WriteLine("Test")).DoInstead(() => { called = true; }).MustBeCalled();
+
             Console.WriteLine("Something");
+
             Assert.IsFalse(called);
         }
     }
